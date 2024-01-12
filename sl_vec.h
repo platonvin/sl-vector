010d255b1189b7debf25dc71b3be2e3f5bfb5242
platonvin <platonvin@gmail.com>
2024-01-13 02:48:31 +0300

    bool vec and few test added

--- sl_vec.h
+++ sl_vec.h
@@ -1,5 +1,5 @@
 #include <tgmath.h>
-
+#include <stdbool.h>
 /*
 C23 standart required (might work without)
 "templates in C"
@@ -45,47 +45,49 @@ typedef union prefix##vec4 {  \
 _create_vec_types(   int, i)
 _create_vec_types( float,  )
 _create_vec_types(double, d)
-_create_vec_types(bool, d)
+_create_vec_types(bool  , b)
 
 #define GET_MACRO(_0, _1, _2, _3, _4, NAME, ...) NAME
 #define _2arg_wrapper(x,y) x,y
 #define _3arg_wrapper(x,y,z) x,y,z
 #define _create_type_error() _assert("WRONG TYPE IN VECTOR CONSTRUCTOR\n", __FILE__, __LINE__)
 
-#define _expand_3types_helper(type1, type2, type3, macro, first_arg, ...) \
+#define _expand_4types_helper(type1, type2, type3, type4, macro, first_arg, ...) \
     type1 : macro(first_arg, type1, __VA_ARGS__),\
     type2 : macro(first_arg, type2, __VA_ARGS__),\
-    type3 : macro(first_arg, type3, __VA_ARGS__)
-#define _expand_3types(type1, type2, type3, macro, first_arg, ...) \
+    type3 : macro(first_arg, type3, __VA_ARGS__),\
+    type4 : macro(first_arg, type4, __VA_ARGS__)
+#define _expand_4types(type1, type2, type3, type4, macro, first_arg, ...) \
     type1 : macro(first_arg, type1, __VA_ARGS__),\
     type2 : macro(first_arg, type2, __VA_ARGS__),\
-    type3 : macro(first_arg, type3, __VA_ARGS__)
+    type3 : macro(first_arg, type3, __VA_ARGS__),\
+    type4 : macro(first_arg, type4, __VA_ARGS__)
 /*======================================================*/
 #define _create_Generic_1d_2args(var, macro, type, ...) \
 _Generic((var),\
-    _expand_3types(int, float, double, macro, type, __VA_ARGS__),\
+    _expand_4types(int, float, double, bool, macro, type, __VA_ARGS__),\
     default : ({_create_type_error();})\
 )
 #define _create_Generic_2d_2args(var, macro, type, ...) \
 _Generic((var),\
-    _expand_3types(ivec2, vec2, dvec2, macro, type, __VA_ARGS__),\
-    _expand_3types(_ivec2, _vec2, _dvec2, macro, type, __VA_ARGS__),\
+    _expand_4types( ivec2,  vec2,  dvec2,  bvec2, macro, type, __VA_ARGS__),\
+    _expand_4types(_ivec2, _vec2, _dvec2, _bvec2, macro, type, __VA_ARGS__),\
     default : ({_create_type_error();})\
 )
 #define _create_Generic_3d_2args(var, macro, type, ...) \
 _Generic((var),\
-    _expand_3types(ivec3, vec3, dvec3, macro, type, __VA_ARGS__),\
+    _expand_4types(ivec3, vec3, dvec3, bvec3, macro, type, __VA_ARGS__),\
     default : ({_create_type_error();})\
 )
 #define _create_Generic_1d_3args(var, macro, _type1, _type2, ...) \
 _Generic((var),\
-    _expand_3types(int, float, double, macro, _2arg_wrapper(_type1, _type2), __VA_ARGS__),\
+    _expand_4types(int, float, double, bool, macro, _2arg_wrapper(_type1, _type2), __VA_ARGS__),\
     default : ({_create_type_error();})\
 )
 #define _create_Generic_2d_3args(var, macro, _type1, _type2, ...) \
 _Generic((var),\
-    _expand_3types(ivec2, vec2, dvec2, macro, _2arg_wrapper(_type1, _type2), __VA_ARGS__),\
-    _expand_3types(_ivec2, _vec2, _dvec2, macro, _2arg_wrapper(_type1, _type2), __VA_ARGS__),\
+    _expand_4types( ivec2,  vec2,  dvec2,  bvec2, macro, _2arg_wrapper(_type1, _type2), __VA_ARGS__),\
+    _expand_4types(_ivec2, _vec2, _dvec2, _bvec2, macro, _2arg_wrapper(_type1, _type2), __VA_ARGS__),\
     default : ({_create_type_error();})\
 )
 
@@ -114,8 +116,8 @@ _Generic((var),\
 typeof(a) a_copy = a; typeof(a)* a_p = &a_copy;\
 typeof(b) b_copy = b; typeof(b)* b_p = &b_copy;\
 _Generic ((a),\
-    _expand_3types_helper(int, float, double, _create_Generic_2d_2args, _2arg_wrapper(b, _3d_from_1dN2d), vec, a_p, b_p),\
-    _expand_3types_helper(ivec2, vec2, dvec2, _create_Generic_1d_2args, _2arg_wrapper(b, _3d_from_2dN1d), vec, a_p, b_p),\
+    _expand_4types_helper(int, float, double, bool, _create_Generic_2d_2args, _2arg_wrapper(b, _3d_from_1dN2d), vec, a_p, b_p),\
+    _expand_4types_helper(ivec2, vec2, dvec2, bvec2, _create_Generic_1d_2args, _2arg_wrapper(b, _3d_from_2dN1d), vec, a_p, b_p),\
     default : ({_create_type_error();})\
 );\
 vec;})
@@ -146,9 +148,9 @@ vec;})
 typeof(a) a_copy = a; typeof(a)* a_p = &a_copy;\
 typeof(b) b_copy = b; typeof(b)* b_p = &b_copy;\
 _Generic ((a),\
-    _expand_3types_helper(int, float, double, _create_Generic_3d_2args, _2arg_wrapper(b, _4d_from_1dN3d), vec, a_p, b_p),\
-    _expand_3types_helper(ivec2, vec2, dvec2, _create_Generic_2d_2args, _2arg_wrapper(b, _4d_from_2dN2d), vec, a_p, b_p),\
-    _expand_3types_helper(ivec3, vec3, dvec3, _create_Generic_1d_2args, _2arg_wrapper(b, _4d_from_3dN1d), vec, a_p, b_p),\
+    _expand_4types_helper(int, float, double, bool, _create_Generic_3d_2args, _2arg_wrapper(b, _4d_from_1dN3d), vec, a_p, b_p),\
+    _expand_4types_helper(ivec2, vec2, dvec2, bvec2, _create_Generic_2d_2args, _2arg_wrapper(b, _4d_from_2dN2d), vec, a_p, b_p),\
+    _expand_4types_helper(ivec3, vec3, dvec3, bvec3, _create_Generic_1d_2args, _2arg_wrapper(b, _4d_from_3dN1d), vec, a_p, b_p),\
     default : ({_create_type_error();})\
 );\
 vec;})
@@ -167,45 +169,45 @@ typeof(b) b_copy = b; typeof(b)* b_p = &b_copy;\
 typeof(c) c_copy = c; typeof(c)* c_p = &c_copy;\
 _Generic ((a),\
     int     : _Generic((b),\
-        _expand_3types_helper(int, float, double, _create_Generic_2d_3args, _3arg_wrapper(c, _4d_from_1dN1dN2d, int), vec, a_p, b_p, c_p),\
-        _expand_3types_helper(ivec2, vec2, dvec2, _create_Generic_1d_3args, _3arg_wrapper(c, _4d_from_1dN2dN1d, int), vec, a_p, b_p, c_p),\
-        _expand_3types_helper(_ivec2, _vec2, _dvec2, _create_Generic_1d_3args, _3arg_wrapper(c, _4d_from_1dN2dN1d, int), vec, a_p, b_p, c_p),\
+        _expand_4types_helper(int, float, double, bool, _create_Generic_2d_3args, _3arg_wrapper(c, _4d_from_1dN1dN2d, int), vec, a_p, b_p, c_p),\
+        _expand_4types_helper( ivec2,  vec2,  dvec2,  bvec2, _create_Generic_1d_3args, _3arg_wrapper(c, _4d_from_1dN2dN1d, int), vec, a_p, b_p, c_p),\
+        _expand_4types_helper(_ivec2, _vec2, _dvec2, _bvec2, _create_Generic_1d_3args, _3arg_wrapper(c, _4d_from_1dN2dN1d, int), vec, a_p, b_p, c_p),\
         default : ({_create_type_error();})\
     ),\
     float : _Generic((b),\
-        _expand_3types_helper(int, float, double, _create_Generic_2d_3args, _3arg_wrapper(c, _4d_from_1dN1dN2d, float), vec, a_p, b_p, c_p),\
-        _expand_3types_helper(ivec2, vec2, dvec2, _create_Generic_1d_3args, _3arg_wrapper(c, _4d_from_1dN2dN1d, float), vec, a_p, b_p, c_p),\
-        _expand_3types_helper(_ivec2, _vec2, _dvec2, _create_Generic_1d_3args, _3arg_wrapper(c, _4d_from_1dN2dN1d, float), vec, a_p, b_p, c_p),\
+        _expand_4types_helper(int, float, double, bool, _create_Generic_2d_3args, _3arg_wrapper(c, _4d_from_1dN1dN2d, float), vec, a_p, b_p, c_p),\
+        _expand_4types_helper( ivec2,  vec2,  dvec2,  bvec2, _create_Generic_1d_3args, _3arg_wrapper(c, _4d_from_1dN2dN1d, float), vec, a_p, b_p, c_p),\
+        _expand_4types_helper(_ivec2, _vec2, _dvec2, _bvec2, _create_Generic_1d_3args, _3arg_wrapper(c, _4d_from_1dN2dN1d, float), vec, a_p, b_p, c_p),\
         default : ({_create_type_error();})\
     ),\
     double : _Generic((b),\
-        _expand_3types_helper(int, float, double, _create_Generic_2d_3args, _3arg_wrapper(c, _4d_from_1dN1dN2d, double), vec, a_p, b_p, c_p),\
-        _expand_3types_helper(ivec2, vec2, dvec2, _create_Generic_1d_3args, _3arg_wrapper(c, _4d_from_1dN2dN1d, double), vec, a_p, b_p, c_p),\
-        _expand_3types_helper(_ivec2, _vec2, _dvec2, _create_Generic_1d_3args, _3arg_wrapper(c, _4d_from_1dN2dN1d, double), vec, a_p, b_p, c_p),\
+        _expand_4types_helper(int, float, double, bool, _create_Generic_2d_3args, _3arg_wrapper(c, _4d_from_1dN1dN2d, double), vec, a_p, b_p, c_p),\
+        _expand_4types_helper( ivec2,  vec2,  dvec2,  bvec2, _create_Generic_1d_3args, _3arg_wrapper(c, _4d_from_1dN2dN1d, double), vec, a_p, b_p, c_p),\
+        _expand_4types_helper(_ivec2, _vec2, _dvec2, _bvec2, _create_Generic_1d_3args, _3arg_wrapper(c, _4d_from_1dN2dN1d, double), vec, a_p, b_p, c_p),\
         default : ({_create_type_error();})\
     ),\
     ivec2 : _Generic((b),\
-        _expand_3types_helper(int, float, double, _create_Generic_1d_3args, _3arg_wrapper(c, _4d_from_2dN1dN1d, ivec2), vec, a_p, b_p, c_p),\
+        _expand_4types_helper(int, float, double, bool, _create_Generic_1d_3args, _3arg_wrapper(c, _4d_from_2dN1dN1d, ivec2), vec, a_p, b_p, c_p),\
         default : ({_create_type_error();})\
     ),\
     _ivec2 : _Generic((b),\
-        _expand_3types_helper(int, float, double, _create_Generic_1d_3args, _3arg_wrapper(c, _4d_from_2dN1dN1d, _ivec2), vec, a_p, b_p, c_p),\
+        _expand_4types_helper(int, float, double, bool, _create_Generic_1d_3args, _3arg_wrapper(c, _4d_from_2dN1dN1d, _ivec2), vec, a_p, b_p, c_p),\
         default : ({_create_type_error();})\
     ),\
      vec2 : _Generic((b),\
-        _expand_3types_helper(int, float, double, _create_Generic_1d_3args, _3arg_wrapper(c, _4d_from_2dN1dN1d, vec2), vec, a_p, b_p, c_p),\
+        _expand_4types_helper(int, float, double, bool, _create_Generic_1d_3args, _3arg_wrapper(c, _4d_from_2dN1dN1d, vec2), vec, a_p, b_p, c_p),\
         default : ({_create_type_error();})\
     ),\
     _vec2 : _Generic((b),\
-        _expand_3types_helper(int, float, double, _create_Generic_1d_3args, _3arg_wrapper(c, _4d_from_2dN1dN1d, _vec2), vec, a_p, b_p, c_p),\
+        _expand_4types_helper(int, float, double, bool, _create_Generic_1d_3args, _3arg_wrapper(c, _4d_from_2dN1dN1d, _vec2), vec, a_p, b_p, c_p),\
         default : ({_create_type_error();})\
     ),\
     dvec2 : _Generic((b),\
-        _expand_3types_helper(int, float, double, _create_Generic_1d_3args, _3arg_wrapper(c, _4d_from_2dN1dN1d, dvec2), vec, a_p, b_p, c_p),\
+        _expand_4types_helper(int, float, double, bool, _create_Generic_1d_3args, _3arg_wrapper(c, _4d_from_2dN1dN1d, dvec2), vec, a_p, b_p, c_p),\
         default : ({_create_type_error();})\
     ),\
     _dvec2 : _Generic((b),\
-        _expand_3types_helper(int, float, double, _create_Generic_1d_3args, _3arg_wrapper(c, _4d_from_2dN1dN1d, _dvec2), vec, a_p, b_p, c_p),\
+        _expand_4types_helper(int, float, double, bool, _create_Generic_1d_3args, _3arg_wrapper(c, _4d_from_2dN1dN1d, _dvec2), vec, a_p, b_p, c_p),\
         default : ({_create_type_error();})\
     ),\
     default : ({_create_type_error();})\
@@ -243,6 +245,15 @@ prefix##vec2 _fun_mul_##prefix##vec2_scale(prefix##vec2 a, typeof((prefix##vec2)
 prefix##vec3 _fun_mul_##prefix##vec3_scale(prefix##vec3 a, typeof((prefix##vec3){0}.x) b)  {return (prefix##vec3) {a.x*b, a.y*b, a.z*b}       ;}\
 prefix##vec4 _fun_mul_##prefix##vec4_scale(prefix##vec4 a, typeof((prefix##vec4){0}.x) b)  {return (prefix##vec4) {a.x*b, a.y*b, a.z*b, a.w*b};}
 
+#define _create_fun_div(prefix) \
+prefix##vec2 _fun_div_##prefix##vec2(prefix##vec2 a, prefix##vec2 b) {return (prefix##vec2) {a.x/b.x, a.y/b.y}                  ;}\
+prefix##vec3 _fun_div_##prefix##vec3(prefix##vec3 a, prefix##vec3 b) {return (prefix##vec3) {a.x/b.x, a.y/b.y, a.z/b.z}         ;}\
+prefix##vec4 _fun_div_##prefix##vec4(prefix##vec4 a, prefix##vec4 b) {return (prefix##vec4) {a.x/b.x, a.y/b.y, a.z/b.z, a.w/b.w};}\
+prefix##vec2 _fun_div_##prefix##vec2_scale(prefix##vec2 a, typeof((prefix##vec2){0}.x) b)  {return (prefix##vec2) {a.x/b, a.y/b}              ;}\
+prefix##vec3 _fun_div_##prefix##vec3_scale(prefix##vec3 a, typeof((prefix##vec3){0}.x) b)  {return (prefix##vec3) {a.x/b, a.y/b, a.z/b}       ;}\
+prefix##vec4 _fun_div_##prefix##vec4_scale(prefix##vec4 a, typeof((prefix##vec4){0}.x) b)  {return (prefix##vec4) {a.x/b, a.y/b, a.z/b, a.w/b};}
+
+
 #define _create_fun_cross(prefix) \
 prefix##vec3 _fun_cross_##prefix##vec3(prefix##vec3 a, prefix##vec3 b)  {return (prefix##vec3) {a.y*b.z - b.y*a.z,  \
                                                                                                 a.z*b.x - b.z*a.x,  \
@@ -268,19 +279,79 @@ prefix##vec2 _fun_normilize_##prefix##vec2(prefix##vec2 a, prefix##vec2 b)  {ret
 prefix##vec3 _fun_normilize_##prefix##vec3(prefix##vec3 a, prefix##vec3 b)  {return (prefix##vec3) {a.x-b.x, a.y-b.y, a.z-b.z}         ;}\
 prefix##vec4 _fun_normilize_##prefix##vec4(prefix##vec4 a, prefix##vec4 b)  {return (prefix##vec4) {a.x-b.x, a.y-b.y, a.z-b.z, a.w-b.w};}
 
-#define _create_funs(name)   \
+#define _create_fun_lessThan(prefix) \
+bvec2 _fun_lessThan_##prefix##vec2(prefix##vec2 a, prefix##vec2 b)  {return (bvec2) {a.x<b.x, a.y<b.y}                  ;}\
+bvec3 _fun_lessThan_##prefix##vec3(prefix##vec3 a, prefix##vec3 b)  {return (bvec3) {a.x<b.x, a.y<b.y, a.z<b.z}         ;}\
+bvec4 _fun_lessThan_##prefix##vec4(prefix##vec4 a, prefix##vec4 b)  {return (bvec4) {a.x<b.x, a.y<b.y, a.z<b.z, a.w<b.w};}
+
+#define _create_fun_lessThanEqual(prefix) \
+bvec2 _fun_lessThanEqual_##prefix##vec2(prefix##vec2 a, prefix##vec2 b)  {return (bvec2) {a.x<=b.x, a.y<=b.y}                  ;}\
+bvec3 _fun_lessThanEqual_##prefix##vec3(prefix##vec3 a, prefix##vec3 b)  {return (bvec3) {a.x<=b.x, a.y<=b.y, a.z<=b.z}         ;}\
+bvec4 _fun_lessThanEqual_##prefix##vec4(prefix##vec4 a, prefix##vec4 b)  {return (bvec4) {a.x<=b.x, a.y<=b.y, a.z<=b.z, a.w<=b.w};}
+
+#define _create_fun_greaterThan(prefix) \
+bvec2 _fun_greaterThan_##prefix##vec2(prefix##vec2 a, prefix##vec2 b)  {return (bvec2) {a.x>b.x, a.y>b.y}                  ;}\
+bvec3 _fun_greaterThan_##prefix##vec3(prefix##vec3 a, prefix##vec3 b)  {return (bvec3) {a.x>b.x, a.y>b.y, a.z>b.z}         ;}\
+bvec4 _fun_greaterThan_##prefix##vec4(prefix##vec4 a, prefix##vec4 b)  {return (bvec4) {a.x>b.x, a.y>b.y, a.z>b.z, a.w>b.w};}
+
+#define _create_fun_greaterThanEqual(prefix) \
+bvec2 _fun_greaterThanEqual_##prefix##vec2(prefix##vec2 a, prefix##vec2 b)  {return (bvec2) {a.x>=b.x, a.y>=b.y}                    ;}\
+bvec3 _fun_greaterThanEqual_##prefix##vec3(prefix##vec3 a, prefix##vec3 b)  {return (bvec3) {a.x>=b.x, a.y>=b.y, a.z>=b.z}          ;}\
+bvec4 _fun_greaterThanEqual_##prefix##vec4(prefix##vec4 a, prefix##vec4 b)  {return (bvec4) {a.x>=b.x, a.y>=b.y, a.z>=b.z, a.w>=b.w};}
+
+#define _create_fun_equal(prefix) \
+bvec2 _fun_equal_##prefix##vec2(prefix##vec2 a, prefix##vec2 b)  {return (bvec2) {a.x==b.x, a.y==b.y}                    ;}\
+bvec3 _fun_equal_##prefix##vec3(prefix##vec3 a, prefix##vec3 b)  {return (bvec3) {a.x==b.x, a.y==b.y, a.z==b.z}          ;}\
+bvec4 _fun_equal_##prefix##vec4(prefix##vec4 a, prefix##vec4 b)  {return (bvec4) {a.x==b.x, a.y==b.y, a.z==b.z, a.w==b.w};}
+
+#define _create_fun_notEqual(prefix) \
+bvec2 _fun_notEqual_##prefix##vec2(prefix##vec2 a, prefix##vec2 b)  {return (bvec2) {a.x!=b.x, a.y!=b.y}                    ;}\
+bvec3 _fun_notEqual_##prefix##vec3(prefix##vec3 a, prefix##vec3 b)  {return (bvec3) {a.x!=b.x, a.y!=b.y, a.z!=b.z}          ;}\
+bvec4 _fun_notEqual_##prefix##vec4(prefix##vec4 a, prefix##vec4 b)  {return (bvec4) {a.x!=b.x, a.y!=b.y, a.z!=b.z, a.w!=b.w};}
+
+#define _create_fun_any(prefix) \
+bool _fun_any_##prefix##vec2(prefix##vec2 vec)  {return (vec.x || vec.y)                  ;}\
+bool _fun_any_##prefix##vec3(prefix##vec3 vec)  {return (vec.x || vec.y || vec.z)         ;}\
+bool _fun_any_##prefix##vec4(prefix##vec4 vec)  {return (vec.x || vec.y || vec.z || vec.w);}
+
+#define _create_fun_all(prefix) \
+bool _fun_all_##prefix##vec2(prefix##vec2 vec)  {return (vec.x && vec.y)                  ;}\
+bool _fun_all_##prefix##vec3(prefix##vec3 vec)  {return (vec.x && vec.y && vec.z)         ;}\
+bool _fun_all_##prefix##vec4(prefix##vec4 vec)  {return (vec.x && vec.y && vec.z && vec.w);}
+
+#define _create_fun_not(prefix) \
+bvec2 _fun_not_##prefix##vec2(prefix##vec2 vec)  {return (bvec2) {!vec.x, !vec.y}                ;}\
+bvec3 _fun_not_##prefix##vec3(prefix##vec3 vec)  {return (bvec3) {!vec.x, !vec.y, !vec.z}        ;}\
+bvec4 _fun_not_##prefix##vec4(prefix##vec4 vec)  {return (bvec4) {!vec.x, !vec.y, !vec.z, !vec.w};}
+
+#define _create_funs(name) \
 _create_fun_##name(i)\
 _create_fun_##name( )\
-_create_fun_##name(d)
+_create_fun_##name(d)\
+_create_fun_##name(b)
 
 _create_funs(add)
 _create_funs(sub)
 _create_funs(mul)
+_create_funs(div)
 _create_funs(dot)
 _create_funs(cross)
 _create_funs(length)
 _create_funs(distance)
 _create_funs(normilize)
+
+_create_funs(lessThan)
+_create_funs(lessThanEqual)
+_create_funs(greaterThan)
+_create_funs(greaterThanEqual)
+_create_funs(equal)
+_create_funs(notEqual)
+
+// _create_funs(notEqual)
+_create_fun_any(b)
+_create_fun_all(b)
+_create_fun_not(b)
+
 
 #define _call_fun(name, vec, ...) _Generic((vec),\
    _ivec2 : _fun_##name##_ivec2,\
@@ -298,6 +369,11 @@ _create_funs(normilize)
     dvec3 : _fun_##name##_dvec3,\
     dvec4 : _fun_##name##_dvec4,\
 \
+   _bvec2 : _fun_##name##_bvec2,\
+    bvec2 : _fun_##name##_bvec2,\
+    bvec3 : _fun_##name##_bvec3,\
+    bvec4 : _fun_##name##_bvec4,\
+\
     default : ({_create_type_error();})\
 )(vec, ## __VA_ARGS__)
 
@@ -307,6 +383,16 @@ _create_funs(normilize)
      vec3 : _fun_##name##_vec3,\
 \
     dvec3 : _fun_##name##_dvec3,\
+\
+    default : ({_create_type_error();})\
+)(vec, ## __VA_ARGS__)
+
+
+#define _call_fun_bool_only(name, vec, ...) _Generic((vec),\
+   _bvec2 : _fun_##name##_bvec2,\
+    bvec2 : _fun_##name##_bvec2,\
+    bvec3 : _fun_##name##_bvec3,\
+    bvec4 : _fun_##name##_bvec4,\
 \
     default : ({_create_type_error();})\
 )(vec, ## __VA_ARGS__)
@@ -359,4 +445,16 @@ _create_funs(normilize)
 #define distance(a, b) _call_fun(distance, a, b)
 #define normilize(vec) _call_fun(normilize, vec)
 #define mul(vec, scaler) _call_fun_vecBYvecORvecBYscale(mul, vec, scaler)
+#define div(vec, scaler) _call_fun_vecBYvecORvecBYscale(div, vec, scaler)
+
+#define lessThan(a, b)         _call_fun(lessThan,         a, b)
+#define lessThanEqual(a, b)    _call_fun(lessThanEqual,    a, b)
+#define greaterThan(a, b)      _call_fun(greaterThan,      a, b)
+#define greaterThanEqual(a, b) _call_fun(greaterThanEqual, a, b)
+#define equal(a, b)            _call_fun(equal,            a, b)
+#define notEqual(a, b)         _call_fun(notEqual,         a, b)
+
+#define any(vec) _call_fun_bool_only(any, vec)
+#define all(vec) _call_fun_bool_only(all, vec)
+#define not(vec) _call_fun_bool_only(not, vec)
 
--- tests.c
+++ tests.c
@@ -3,13 +3,61 @@
 
 #include "sl_vec.h"
 
-// #define printf_vec(vec) _Generic((vec),
-//     _expand_3types_helper()
-// )
+/*
+printf macro might be useful
+*/
+
+#define _printf_vec2d(prefix, specifier, vec) printf(specifier " " specifier "\n", (*((prefix##vec2*)(&vec))).x, (*((prefix##vec2*)(&vec))).y)
+#define _printf_vec3d(prefix, specifier, vec) printf(specifier " " specifier " " specifier "\n", (*((prefix##vec3*)(&vec))).x, (*((prefix##vec3*)(&vec))).y, (*((prefix##vec3*)(&vec))).z)
+#define _printf_vec4d(prefix, specifier, vec) printf(specifier " " specifier " " specifier " " specifier "\n", (*((prefix##vec4*)(&vec))).x, (*((prefix##vec4*)(&vec))).y, (*((prefix##vec4*)(&vec))).z, (*((prefix##vec4*)(&vec))).w)
+
+#define printf_vec(vec) \
+({typeof(vec) vec_copy = vec;\
+_Generic((vec),\
+   _ivec2 : _printf_vec2d(i, "%d", vec_copy),\
+    ivec2 : _printf_vec2d(i, "%d", vec_copy),\
+    ivec3 : _printf_vec3d(i, "%d", vec_copy),\
+    ivec4 : _printf_vec4d(i, "%d", vec_copy),\
+   _vec2  : _printf_vec2d( , "%.3f", vec_copy),\
+    vec2  : _printf_vec2d( , "%.3f", vec_copy),\
+    vec3  : _printf_vec3d( , "%.3f", vec_copy),\
+    vec4  : _printf_vec4d( , "%.3f", vec_copy),\
+   _dvec2 : _printf_vec2d(d, "%.3lf", vec_copy),\
+    dvec2 : _printf_vec2d(d, "%.3lf", vec_copy),\
+    dvec3 : _printf_vec3d(d, "%.3lf", vec_copy),\
+    dvec4 : _printf_vec4d(d, "%.3lf", vec_copy),\
+   _bvec2 : _printf_vec2d(b, "%d", vec_copy),\
+    bvec2 : _printf_vec2d(b, "%d", vec_copy),\
+    bvec3 : _printf_vec3d(b, "%d", vec_copy),\
+    bvec4 : _printf_vec4d(b, "%d", vec_copy),\
+    default : _create_type_error()\
+);\
+vec_copy;})
 
 int main()
 {
+    bvec2 b00 = {0,0};
+    bvec2 b01 = {0,1};
+    bvec2 b10 = {1,0};
+    bvec2 b11 = {1,1};
 
-    
+    assert(!any(b00));
+    assert(any(not(b00)));
+    assert(all(not(b00)));
+
+    assert(all(greaterThan(vec4(2,2,2,2).xyz, vec4(1,1,1,999).xyz)));
+    assert(all(equal(ivec4(2,2,2,2).yz, ivec4(-666,2,2,999).yz)));
+    assert(all(notEqual(dvec4(2,2.1,2.1,2).yz, dvec3(2,2.2,2.2).yz)));
+
+    assert(all(equal(ivec2(5,5), add(ivec2(2,2), ivec2(3,3)))));
+    assert(all(equal(ivec2(4,4), mul(ivec2(2,2), 2))));
+    assert(all(equal(ivec2(4,4), mul(ivec2(2,2), ivec2(2,2)))));
+    assert(all(equal(ivec2(4,4), div(ivec2(8,8), 2))));
+    assert(all(equal(ivec2(4,4), div(ivec2(8,8), ivec2(2,2)))));
+
+    assert(dot(ivec2(1,1), div(ivec2(9,9), 2)) == 8);
+    printf("%d\n", length(mul(ivec2(1,1), div(ivec2(9,9), 2))) );
+    assert(length(mul(ivec2(1,1), div(ivec2(9,9), 2))) == 5);
+
     return 0;
 }
